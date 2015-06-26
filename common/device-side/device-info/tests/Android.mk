45c863c975cedbb29d56ed09932175d214e0ca26
Agatha Man <agathaman@google.com>
2015-06-26 13:26:04 -0700

    Test for DeviceInfoActivity API
    Change-Id: I913cedf54cf92a3f56160b65dd8d3a04662ef196

--- common/device-side/device-info/tests/Android.mk
+++ common/device-side/device-info/tests/Android.mk
@@ -20,6 +20,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_STATIC_JAVA_LIBRARIES := compatibility-device-info
 
+LOCAL_JAVA_LIBRARIES := android.test.runner
+
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE := compatibility-device-info-tests
