486c6f8c0bc875c3f443f1689c63313f8b6fd9c1
Nash Lincoln <nashira@google.com>
2015-09-17 11:08:55 -0700

    migrate libcorelegacy22 tests to cts_v2
    bug:21762834
    Change-Id: I6c9e796c351a127fa8173462938ac19daf51173b

--- tests/tests/libcorelegacy22/Android.mk
+++ tests/tests/libcorelegacy22/Android.mk
@@ -29,4 +29,7 @@ LOCAL_PACKAGE_NAME := CtsLibcoreLegacy22TestCases
 
 LOCAL_SDK_VERSION := 22
 
+# Tag this module as a cts_v2 test artifact
+LOCAL_COMPATIBILITY_SUITE := cts_v2
+
 include $(BUILD_CTS_PACKAGE)
