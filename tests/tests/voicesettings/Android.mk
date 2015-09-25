ebc2ea928e9e2ceaeb11e68bb5ae8c051aebb931
Aaron Holden <aaronholden@google.com>
2015-09-24 17:12:23 -0700

    CTS_v2 migration for voicesettings tests
    Change-Id: I1de231be3d8304340955e4f7f0ef5bc8708b95ef

--- tests/tests/voicesettings/Android.mk
+++ tests/tests/voicesettings/Android.mk
@@ -27,6 +27,11 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := CtsVoiceSettingsTestCases
 
+# Tag this module as a cts_v2 test artifact
+LOCAL_COMPATIBILITY_SUITE := cts_v2
+
+LOCAL_CTS_MODULE_CONFIG := $(LOCAL_PATH)/Old$(CTS_MODULE_TEST_CONFIG)
+
 LOCAL_SDK_VERSION := current
 
 include $(BUILD_CTS_PACKAGE)
