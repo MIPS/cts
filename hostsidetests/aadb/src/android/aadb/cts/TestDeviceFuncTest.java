8cc910a6b8788757e4bb7f5d131beb0f8f7657f9
Aaron Holden <aaronholden@google.com>
2015-10-16 17:09:31 -0700

    Migrate aadb tests to CTS_v2
    bug:21762834
    Change-Id: I486fc9dec939d9d27d1105fb1f3805e3a475de49

--- hostsidetests/aadb/src/android/aadb/cts/TestDeviceFuncTest.java
+++ hostsidetests/aadb/src/android/aadb/cts/TestDeviceFuncTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.cts.aadb;
+package android.aadb.cts;
 
 import com.android.ddmlib.IDevice;
 import com.android.ddmlib.Log;
