19a17c9e24e10d722e9462ac1f99238e19105aab
Nash Lincoln <nashira@google.com>
2015-11-06 16:09:26 -0800

    migrate host atrace tests to cts_v2
    Change-Id: I932a1126d2d47b64f536a7de428e2ba5a6b6d0c6

--- hostsidetests/atrace/AtraceTestApp/Android.mk
+++ hostsidetests/atrace/AtraceTestApp/Android.mk
@@ -30,4 +30,7 @@ LOCAL_PACKAGE_NAME := CtsAtraceTestApp
 
 #LOCAL_DEX_PREOPT := false
 
+# Tag this module as a cts_v2 test artifact
+LOCAL_COMPATIBILITY_SUITE := cts_v2
+
 include $(BUILD_CTS_SUPPORT_PACKAGE)
