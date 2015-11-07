f921ac3d62c8257b15d1bd1f4555e5b409c7ac75
Nash Lincoln <nashira@google.com>
2015-11-06 16:54:38 -0800

    migrate monkey host test to cts_v2
    Change-Id: Iaf5630f9d8faa38fcca2a5f0c19a9026ba9f587a

--- hostsidetests/monkey/Android.mk
+++ hostsidetests/monkey/Android.mk
@@ -22,10 +22,15 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE := CtsMonkeyTestCases
 
-LOCAL_JAVA_LIBRARIES := cts-tradefed tradefed-prebuilt
+LOCAL_JAVA_LIBRARIES := cts-tradefed_v2 compatibility-host-util tradefed-prebuilt
+
+LOCAL_STATIC_JAVA_LIBRARIES := cts-migration-lib
 
 # prefix zzz intentional to run this last
 LOCAL_CTS_TEST_PACKAGE := zzz.android.monkey
+
+# tag this module as a cts_v2 test artifact
+LOCAL_COMPATIBILITY_SUITE := cts_v2
 
 include $(BUILD_CTS_HOST_JAVA_LIBRARY)
 
