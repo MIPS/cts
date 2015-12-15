95753f99e7bab11ffe530fde3a5335356be4fae9
Aaron Holden <aaronholden@google.com>
2015-12-15 15:53:47 -0800

    Remove unnecessary cts_v2 tag from hardware module
    Change-Id: Ib0ebf5e742cd19ffaef9962abd3414bb23480b89

--- tests/tests/hardware/Android.mk
+++ tests/tests/hardware/Android.mk
@@ -24,9 +24,6 @@ LOCAL_MODULE_TAGS := tests
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
 
-# Tag this module as a cts_v2 test artifact
-LOCAL_COMPATIBILITY_SUITE := cts_v2
-
 LOCAL_STATIC_JAVA_LIBRARIES := ctsdeviceutil compatibility-device-util
 
 LOCAL_SDK_VERSION := current
