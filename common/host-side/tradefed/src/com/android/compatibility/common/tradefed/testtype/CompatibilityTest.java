33d43e1c573d4d189613d17a7682211e3be9954e
Stuart Scott <stuartscott@google.com>
2015-12-22 10:27:26 -0800

    Only the thread that initialized the repo should tear it down.
    bug:21762834
    Change-Id: I4a0645376a1063ad10fc087f831f5ee99ac6c45e

--- common/host-side/tradefed/src/com/android/compatibility/common/tradefed/testtype/CompatibilityTest.java
+++ common/host-side/tradefed/src/com/android/compatibility/common/tradefed/testtype/CompatibilityTest.java
@@ -221,11 +221,13 @@ public class CompatibilityTest implements IDeviceTest, IShardableTest, IBuildRec
     @Override
     public void run(ITestInvocationListener listener) throws DeviceNotAvailableException {
         try {
+            boolean isInitializer = false;
             IModuleRepo moduleRepo = ModuleRepo.getInstance();
             // Synchronized so only one shard enters and sets up the moduleRepo. When the other
             // shards enter after this, moduleRepo is already initialized so they dont do anything
             synchronized (moduleRepo) {
                 if (!moduleRepo.isInitialized()) {
+                    isInitializer = true;
                     setupFilters();
                     // Initialize the repository, {@link CompatibilityBuildHelper#getTestsDir} can
                     // throw a {@link FileNotFoundException}
@@ -262,7 +264,9 @@ public class CompatibilityTest implements IDeviceTest, IShardableTest, IBuildRec
         } catch (Error e) {
             CLog.e(e);
         } finally {
-            ModuleRepo.tearDown();
+            if (isInitializer) {
+                ModuleRepo.tearDown();
+            }
         }
     }
 
