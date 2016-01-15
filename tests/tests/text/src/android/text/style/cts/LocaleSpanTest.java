92369b1c98e64b047e4ca3411b98a8f013e8015a
Yohei Yukawa <yukawa@google.com>
2016-01-15 13:43:00 -0800

    Cover LocaleSpan in CTS.
    This CL mainly covers LocaleSpan#getLocales(), which is being added [1].
    
      [1]: Ib754796d399cd00f9d02005df88874a5cd5c7642
           c38b51953921c17181fbe334cdaa95f188bfdb14
    
    It also covers the constructor of LocaleSpan and LocaleSpan#getLocale()
    indirectly.
    
    Bug: 26567158
    Change-Id: I60e8e55e1c4340fbfa4a5eecae53515dda604b92

---
+++ tests/tests/text/src/android/text/style/cts/LocaleSpanTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.text.style.cts;
+
+import junit.framework.TestCase;
+
+import android.annotation.NonNull;
+import android.os.Parcel;
+import android.text.style.LocaleSpan;
+import android.util.LocaleList;
+
+public class LocaleSpanTest extends TestCase {
+
+    public void testGetLocalesMain(@NonNull final LocaleList locales) {
+        final LocaleSpan span = new LocaleSpan(locales);
+        assertEquals(locales.getPrimary(), span.getLocale());
+        assertEquals(locales, span.getLocales());
+
+        final LocaleSpan cloned = cloneViaParcel(span);
+        assertEquals(locales.getPrimary(), cloned.getLocale());
+        assertEquals(locales, cloned.getLocales());
+    }
+
+    public void testGetLocales() {
+        testGetLocalesMain(LocaleList.getEmptyLocaleList());
+        testGetLocalesMain(LocaleList.forLanguageTags("en"));
+        testGetLocalesMain(LocaleList.forLanguageTags("en-GB,en"));
+        testGetLocalesMain(LocaleList.forLanguageTags("de-DE-u-co-phonebk,en-GB,en"));
+    }
+
+    public void testConstructorWithLocaleList() {
+        try {
+            new LocaleSpan((LocaleList) null);
+        } catch (NullPointerException e) {
+            // Expected.
+            return;
+        }
+        fail("NullPointerException must have been thrown.");
+    }
+
+    @NonNull
+    LocaleSpan cloneViaParcel(@NonNull final LocaleSpan original) {
+        Parcel parcel = null;
+        try {
+            parcel = Parcel.obtain();
+            original.writeToParcel(parcel, 0);
+            parcel.setDataPosition(0);
+            return new LocaleSpan(parcel);
+        } finally {
+            if (parcel != null) {
+                parcel.recycle();
+            }
+        }
+    }
+}
