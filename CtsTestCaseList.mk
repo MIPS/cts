3a1663d67c70a5ed6496167f18c43947d59245ca
Robert Sesek <rsesek@google.com>
2016-01-21 18:25:39 -0500

    Add a CTS test for BIND_EXTERNAL_SERVICE.
    Bug: 22084679
    Bug: 21643067
    Change-Id: I7b72e2fefd9b4ee6fac989026b8a50c1f8be307a

--- CtsTestCaseList.mk
+++ CtsTestCaseList.mk
@@ -95,6 +95,7 @@ cts_support_packages := \
     CtsDeviceTaskSwitchingAppA \
     CtsDeviceTaskSwitchingAppB \
     CtsDeviceTaskSwitchingControl \
+    CtsExternalServiceService \
     CtsHostsideNetworkTestsApp \
     CtsIntentReceiverApp \
     CtsIntentSenderApp \
@@ -159,6 +160,7 @@ cts_test_packages := \
     CtsDreamsTestCases \
     CtsDrmTestCases \
     CtsEffectTestCases \
+    CtsExternalServiceTestCases \
     CtsFileSystemTestCases \
     CtsGestureTestCases \
     CtsGraphicsTestCases \
