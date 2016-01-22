10ed08322fe50534aa3f9a1f055b28c473d9fe15
Felipe Leme <felipeal@google.com>
2016-01-22 15:44:21 -0800

    Added CTS tests for ACTION_OPEN_EXTERNAL_DIRECTORY.
    BUG: 23011462
    Change-Id: I2a87b31f47a83023b71944d8db545eb19dae8bfa

--- hostsidetests/appsecurity/test-apps/DocumentClient/src/com/android/cts/documentclient/DocumentsClientTest.java
+++ hostsidetests/appsecurity/test-apps/DocumentClient/src/com/android/cts/documentclient/DocumentsClientTest.java
@@ -16,21 +16,46 @@
 
 package com.android.cts.documentclient;
 
+import static android.os.Environment.DIRECTORY_ALARMS;
+import static android.os.Environment.DIRECTORY_DCIM;
+import static android.os.Environment.DIRECTORY_DOCUMENTS;
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
+import static android.os.Environment.DIRECTORY_MOVIES;
+import static android.os.Environment.DIRECTORY_MUSIC;
+import static android.os.Environment.DIRECTORY_NOTIFICATIONS;
+import static android.os.Environment.DIRECTORY_PICTURES;
+import static android.os.Environment.DIRECTORY_PODCASTS;
+import static android.os.Environment.DIRECTORY_RINGTONES;
+import static android.os.Environment.getExternalStorageDirectory;
+import static android.test.MoreAsserts.assertContainsRegex;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Environment;
 import android.os.SystemClock;
 import android.provider.DocumentsContract;
 import android.provider.DocumentsContract.Document;
 import android.provider.DocumentsProvider;
+import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.Configurator;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObject;
 import android.support.test.uiautomator.UiObjectNotFoundException;
 import android.support.test.uiautomator.UiScrollable;
 import android.support.test.uiautomator.UiSelector;
+import android.support.test.uiautomator.Until;
 import android.test.InstrumentationTestCase;
 import android.test.MoreAsserts;
 import android.text.format.DateUtils;
@@ -38,12 +63,6 @@ import android.util.Log;
 import android.view.MotionEvent;
 
 import com.android.cts.documentclient.MyActivity.Result;
-
-import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 /**
  * Tests for {@link DocumentsProvider} and interaction with platform intents
@@ -56,6 +75,21 @@ public class DocumentsClientTest extends InstrumentationTestCase {
     private MyActivity mActivity;
 
     private static final long TIMEOUT = 30 * DateUtils.SECOND_IN_MILLIS;
+
+    private static final int REQUEST_CODE = 42;
+
+    private static final String[] STANDARD_DIRECTORIES = {
+        DIRECTORY_MUSIC,
+        DIRECTORY_PODCASTS,
+        DIRECTORY_RINGTONES,
+        DIRECTORY_ALARMS,
+        DIRECTORY_NOTIFICATIONS,
+        DIRECTORY_PICTURES,
+        DIRECTORY_MOVIES,
+        DIRECTORY_DOWNLOADS,
+        DIRECTORY_DCIM,
+        DIRECTORY_DOCUMENTS
+    };
 
     @Override
     public void setUp() throws Exception {
@@ -130,7 +164,7 @@ public class DocumentsClientTest extends InstrumentationTestCase {
         final Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("*/*");
-        mActivity.startActivityForResult(intent, 42);
+        mActivity.startActivityForResult(intent, REQUEST_CODE);
 
         // Ensure that we see both of our roots
         mDevice.waitForIdle();
@@ -170,7 +204,7 @@ public class DocumentsClientTest extends InstrumentationTestCase {
 
         final Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.setType("*/*");
-        mActivity.startActivityForResult(intent, 42);
+        mActivity.startActivityForResult(intent, REQUEST_CODE);
 
         // Pick a virtual file from the local root.
         mDevice.waitForIdle();
@@ -195,7 +229,7 @@ public class DocumentsClientTest extends InstrumentationTestCase {
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.putExtra(Intent.EXTRA_TITLE, DISPLAY_NAME);
         intent.setType(MIME_TYPE);
-        mActivity.startActivityForResult(intent, 42);
+        mActivity.startActivityForResult(intent, REQUEST_CODE);
 
         mDevice.waitForIdle();
         findRoot("CtsCreate").click();
@@ -219,7 +253,7 @@ public class DocumentsClientTest extends InstrumentationTestCase {
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.putExtra(Intent.EXTRA_TITLE, "NEVERUSED");
         intent.setType("mime2/file2");
-        mActivity.startActivityForResult(intent, 42);
+        mActivity.startActivityForResult(intent, REQUEST_CODE);
 
         mDevice.waitForIdle();
         findRoot("CtsCreate").click();
@@ -244,7 +278,7 @@ public class DocumentsClientTest extends InstrumentationTestCase {
         if (!supportedHardware()) return;
 
         final Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-        mActivity.startActivityForResult(intent, 42);
+        mActivity.startActivityForResult(intent, REQUEST_CODE);
 
         mDevice.waitForIdle();
         findRoot("CtsCreate").click();
@@ -312,13 +346,110 @@ public class DocumentsClientTest extends InstrumentationTestCase {
         }
     }
 
+    // Not called by hostside tests, but useful during development...
+    public void testOpenExternalDirectory() throws Exception {
+        testOpenExternalDirectory_invalidPath();
+        testOpenExternalDirectory_userRejects();
+        testOpenExternalDirectory_userAccepts();
+    }
+
+    public void testOpenExternalDirectory_invalidPath() throws Exception {
+        if (!supportedHardware()) return;
+
+        final String externalRoot = getExternalStorageDirectory().getPath();
+        openExternalDirectoryInvalidPath("");
+        openExternalDirectoryInvalidPath("/dev/null");
+        openExternalDirectoryInvalidPath(externalRoot + "/../");
+        openExternalDirectoryInvalidPath(externalRoot + "/HiddenStuff");
+    }
+
+    public void testOpenExternalDirectory_userRejects() throws Exception {
+        if (!supportedHardware()) return;
+
+        final String externalRoot = getExternalStorageDirectory().getPath();
+
+        // Tests user clicking DENY button, for all valid directories.
+        for (String directory : STANDARD_DIRECTORIES) {
+            final Uri uri = Uri.fromFile(new File(externalRoot, directory));
+
+            final UiAlertDialog dialog = openExternalDirectoryValidPath(uri);
+            dialog.noButton.click();
+            assertActivityFailed();
+        }
+
+        // Also test user clicking back button - one directory is enough.
+        openExternalDirectoryValidPath(Uri.fromFile(new File(externalRoot, DIRECTORY_PICTURES)));
+        mDevice.pressBack();
+        assertActivityFailed();
+    }
+
+    public void testOpenExternalDirectory_userAccepts() throws Exception {
+        if (!supportedHardware())
+            return;
+
+        // TODO: once there is an API to get all volumes, use a for loop to call method below
+        // to all of them (rather than hard-coding).
+        userAcceptsOpenExternalDirectoryTest(getExternalStorageDirectory().getPath(),
+                "Internal storage");
+    }
+
+    private void userAcceptsOpenExternalDirectoryTest(String rootPath, String rootDescription)
+            throws Exception {
+        final Uri requestedUri = Uri.fromFile(new File(rootPath, DIRECTORY_PICTURES));
+
+        // Asserts dialog contain the proper message.
+        final UiAlertDialog dialog = openExternalDirectoryValidPath(requestedUri);
+        final String message = dialog.messageText.getText();
+        Log.v(TAG, "request permission message: " + message);
+        final Context context = getInstrumentation().getTargetContext();
+        final String appLabel = context.getPackageManager().getApplicationLabel(
+                context.getApplicationInfo()).toString();
+        assertContainsRegex("missing app label", appLabel, message);
+        assertContainsRegex("missing folder", DIRECTORY_PICTURES, message);
+        assertContainsRegex("missing root", rootDescription, message);
+
+        // Call API...
+        dialog.yesButton.click();
+
+        // ...and get its response.
+        final Intent data = assertActivitySucceeded();
+        final Uri grantedUri = data.getData();
+
+        // Test granted permission directly by persisting it...
+        final ContentResolver resolver = getInstrumentation().getContext().getContentResolver();
+        final int modeFlags = data.getFlags()
+                & (Intent.FLAG_GRANT_READ_URI_PERMISSION
+                | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        resolver.takePersistableUriPermission(grantedUri, modeFlags);
+
+        // ...and indirectly by creating some documents
+        final Uri doc = DocumentsContract.buildDocumentUriUsingTree(grantedUri,
+                DocumentsContract.getTreeDocumentId(grantedUri));
+        assertNotNull("could not get tree UURI", doc);
+        final Uri pic = DocumentsContract.createDocument(resolver, doc, "image/png", "pic.png");
+        assertNotNull("could not create file (pic.png) on tree root", pic);
+        final Uri dir = DocumentsContract.createDocument(resolver, doc, Document.MIME_TYPE_DIR,
+                "my dir");
+        assertNotNull("could not create child dir (my dir)", pic);
+        final Uri dirPic = DocumentsContract.createDocument(resolver, dir, "image/png", "pic2.png");
+        assertNotNull("could not create file (pic.png) on child dir (my dir)", dirPic);
+
+        writeFully(pic, "pic".getBytes());
+        writeFully(dirPic, "dirPic".getBytes());
+
+        // Clean up created documents.
+        assertTrue("delete", DocumentsContract.deleteDocument(resolver, pic));
+        assertTrue("delete", DocumentsContract.deleteDocument(resolver, dirPic));
+        assertTrue("delete", DocumentsContract.deleteDocument(resolver, dir));
+    }
+
     public void testGetContent() throws Exception {
         if (!supportedHardware()) return;
 
         final Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("*/*");
-        mActivity.startActivityForResult(intent, 42);
+        mActivity.startActivityForResult(intent, REQUEST_CODE);
 
         // Look around, we should be able to see both DocumentsProviders and
         // other GET_CONTENT sources.
@@ -378,5 +509,54 @@ public class DocumentsClientTest extends InstrumentationTestCase {
             return false;
         }
         return true;
+    }
+
+    private void assertActivityFailed() {
+        final Result result = mActivity.getResult();
+        assertEquals(REQUEST_CODE, result.requestCode);
+        assertEquals(Activity.RESULT_CANCELED, result.resultCode);
+        assertNull(result.data);
+    }
+
+    private Intent assertActivitySucceeded() {
+        final Result result = mActivity.getResult();
+        assertEquals(REQUEST_CODE, result.requestCode);
+        assertEquals(Activity.RESULT_OK, result.resultCode);
+        assertNotNull(result.data);
+        return result.data;
+    }
+
+    private void openExternalDirectoryInvalidPath(String path) {
+        sendOpenExternalDirectoryIntent(Uri.fromFile(new File(path)));
+        assertActivityFailed();
+    }
+
+    private UiAlertDialog openExternalDirectoryValidPath(Uri uri) throws UiObjectNotFoundException {
+        sendOpenExternalDirectoryIntent(uri);
+        return new UiAlertDialog();
+    }
+
+    private void sendOpenExternalDirectoryIntent(Uri uri) {
+        final Intent intent = new Intent(Intent.ACTION_OPEN_EXTERNAL_DIRECTORY);
+        intent.setData(uri);
+        mActivity.startActivityForResult(intent, REQUEST_CODE);
+        mDevice.waitForIdle();
+    }
+
+    private final class UiAlertDialog {
+        final UiObject messageText;
+        final UiObject yesButton;
+        final UiObject noButton;
+
+        UiAlertDialog() throws UiObjectNotFoundException {
+            final String id = "android:id/parentPanel";
+            boolean gotIt = mDevice.wait(Until.hasObject(By.res(id)), TIMEOUT);
+            assertTrue("object with id '(" + id + "') not visible yet", gotIt);
+            final UiObject dialog = mDevice.findObject(new UiSelector().resourceId(id));
+            assertTrue("object with id '(" + id + "') doesn't exist", dialog.exists());
+            messageText = dialog.getChild(new UiSelector().resourceId("android:id/message"));
+            yesButton = dialog.getChild(new UiSelector().resourceId("android:id/button1"));
+            noButton  = dialog.getChild(new UiSelector().resourceId("android:id/button2"));
+        }
     }
 }
