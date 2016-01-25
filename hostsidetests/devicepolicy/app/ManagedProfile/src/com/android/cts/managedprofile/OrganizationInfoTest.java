58336821d4ad4935fa42197b64c87cc1ec8b7679
Robin Lee <rgl@google.com>
2016-01-25 16:21:38 +0000

    Tests for set/get organization color
    Checks that the default colour is indeed Color.GRAY and that any of a
    selection of common colours or one arbitrary colour can be set.
    
    Bug: 26638631
    Change-Id: I0c5ed01e7cfc050ada34d43baa804a3041ff22d0

---
+++ hostsidetests/devicepolicy/app/ManagedProfile/src/com/android/cts/managedprofile/OrganizationInfoTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.cts.managedprofile;
+
+import android.graphics.Color;
+
+public class OrganizationInfoTest extends BaseManagedProfileTest {
+
+    public void testDefaultOrganizationColorIsGray() {
+        int defaultColor = mDevicePolicyManager.getOrganizationColor(ADMIN_RECEIVER_COMPONENT);
+        assertEquals(Color.GRAY, defaultColor);
+    }
+
+    public void testSetOrganizationColor() {
+        int previousColor = mDevicePolicyManager.getOrganizationColor(ADMIN_RECEIVER_COMPONENT);
+
+        try {
+            final int[] colors = {
+                Color.TRANSPARENT,
+                Color.WHITE,
+                Color.RED,
+                Color.GREEN,
+                Color.BLUE,
+                0x7FFE5B35 /* HTML name: "Sunset orange". Opacity: 50%. */
+            };
+
+            for (int color : colors) {
+                mDevicePolicyManager.setOrganizationColor(ADMIN_RECEIVER_COMPONENT, color);
+                assertEquals(color,
+                        mDevicePolicyManager.getOrganizationColor(ADMIN_RECEIVER_COMPONENT));
+            }
+        } finally {
+            // Put the organization color back how it was.
+            mDevicePolicyManager.setOrganizationColor(ADMIN_RECEIVER_COMPONENT, previousColor);
+        }
+    }
+
+    public void testSetOrGetOrganizationColorWithNullAdminFails() {
+        try {
+            mDevicePolicyManager.setOrganizationColor(null, Color.GRAY);
+            fail("Exception should have been thrown for null admin ComponentName");
+        } catch (NullPointerException expected) {
+        }
+
+        try {
+            int color = mDevicePolicyManager.getOrganizationColor(null);
+            fail("Exception should have been thrown for null admin ComponentName");
+        } catch (NullPointerException expected) {
+        }
+    }
+}
