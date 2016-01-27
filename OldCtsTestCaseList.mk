85796c9e16247c52c124455ee00ec3428bc73ee8
Chad Brubaker <cbrubaker@google.com>
2016-01-27 10:33:53 -0800

    Cherry-pick cleartext blocking CTS tests
    The tests landed in master after the nyc-dev cut.
    
    Bug:27293197
    (cherry-picked from commits b617aceda80553cb25e7221e226d8a414f11c5c9,
    dc77100fc7556da219fbd86c80a479be212bfdca)
    
    Change-Id: Ifc96f8c13a3f42f27f129b9edaf7ac41515f95d5

--- OldCtsTestCaseList.mk
+++ OldCtsTestCaseList.mk
@@ -195,6 +195,7 @@ cts_test_packages := \
     CtsNetTestCasesLegacyApi22 \
     CtsNetTestCasesLegacyPermission22 \
     CtsNetSecConfigAttributeTestCases \
+    CtsNetSecConfigCleartextTrafficTestCases \
     CtsNetSecConfigBasicDebugDisabledTestCases \
     CtsNetSecConfigBasicDebugEnabledTestCases \
     CtsNetSecConfigBasicDomainConfigTestCases \
