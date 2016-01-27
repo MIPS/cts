3fb5b364028955b4f4a76dee45cd15843ca088de
Vladislav Kaznacheev <kaznacheev@google.com>
2016-01-27 12:00:48 -0800

    Add more CTS tests for cross-app drag and drop
    Separate tests for each of the four flags controlling
    the permission grants (read, write, prefix, persistable).
    
    Test that global drag and drop is not happening unless it is
    explicitly enabled.
    
    Test View.cancelDragAndDrop.
    
    Bug: 25375629
    Change-Id: I3beb8f678a026545d8996efb147d3b9415be189e

--- tests/tests/draganddrop/dragsource/src/android/dnd/cts/dragsource/DragSource.java
+++ tests/tests/draganddrop/dragsource/src/android/dnd/cts/dragsource/DragSource.java
@@ -20,13 +20,15 @@ import android.app.Activity;
 import android.content.ClipData;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
 public class DragSource extends Activity{
     private static final String URI_PREFIX =
-            "content://" + DragSourceContentProvider.AUTHORITY + "/data/";
+            "content://" + DragSourceContentProvider.AUTHORITY + "/data";
 
     private static final String MAGIC_VALUE = "42";
+    public static final long TIMEOUT_CANCEL = 150;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,24 +37,47 @@ public class DragSource extends Activity{
         View view = getLayoutInflater().inflate(R.layout.main_activity, null);
         setContentView(view);
 
-        setUpDragSource(R.id.dont_grant, false);
-        setUpDragSource(R.id.do_grant, true);
+        final Uri plainUri = Uri.parse(URI_PREFIX + "/" + MAGIC_VALUE);
+
+        setUpDragSource(R.id.disallow_global, plainUri, 0);
+        setUpDragSource(R.id.cancel_soon, plainUri, View.DRAG_FLAG_GLOBAL);
+
+        setUpDragSource(R.id.dont_grant, plainUri, View.DRAG_FLAG_GLOBAL);
+        setUpDragSource(R.id.grant_read, plainUri,
+                View.DRAG_FLAG_GLOBAL | View.DRAG_FLAG_GLOBAL_URI_READ);
+        setUpDragSource(R.id.grant_write, plainUri,
+                View.DRAG_FLAG_GLOBAL | View.DRAG_FLAG_GLOBAL_URI_WRITE);
+        setUpDragSource(R.id.grant_read_persistable, plainUri,
+                View.DRAG_FLAG_GLOBAL | View.DRAG_FLAG_GLOBAL_URI_READ |
+                        View.DRAG_FLAG_GLOBAL_PERSISTABLE_URI_PERMISSION);
+
+        final Uri prefixUri = Uri.parse(URI_PREFIX);
+
+        setUpDragSource(R.id.grant_read_prefix, prefixUri,
+                View.DRAG_FLAG_GLOBAL | View.DRAG_FLAG_GLOBAL_URI_READ |
+                        View.DRAG_FLAG_GLOBAL_PREFIX_URI_PERMISSION);
+        setUpDragSource(R.id.grant_read_noprefix, prefixUri,
+                View.DRAG_FLAG_GLOBAL | View.DRAG_FLAG_GLOBAL_URI_READ);
+
     }
 
-    private void setUpDragSource(final int resourceId, final boolean grantPermissions) {
+    private void setUpDragSource(final int resourceId, final Uri uri, final int flags) {
         findViewById(resourceId).setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public boolean onLongClick(View v) {
-                int flags = View.DRAG_FLAG_GLOBAL;
-                if (grantPermissions) {
-                    flags |= View.DRAG_FLAG_GLOBAL_URI_READ | View.DRAG_FLAG_GLOBAL_URI_WRITE;
-                }
-                final Uri uri = Uri.parse(URI_PREFIX + MAGIC_VALUE);
+            public boolean onLongClick(final View v) {
                 v.startDragAndDrop(
                         ClipData.newUri(getContentResolver(), "", uri),
                         new View.DragShadowBuilder(v),
                         null,
                         flags);
+                if (resourceId == R.id.cancel_soon) {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            v.cancelDragAndDrop();
+                        }
+                    }, TIMEOUT_CANCEL);
+                }
                 return false;
             }
         });
--- tests/tests/draganddrop/droptarget/src/android/dnd/cts/droptarget/DropTarget.java
+++ tests/tests/draganddrop/droptarget/src/android/dnd/cts/droptarget/DropTarget.java
@@ -18,6 +18,7 @@ package android.dnd.cts.droptarget;
 
 import android.app.Activity;
 import android.content.ClipData;
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -29,6 +30,7 @@ import android.widget.TextView;
 public class DropTarget extends Activity {
 
     private static final String MAGIC_VALUE = "42";
+    public static final String RESULT_OK = "OK";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,93 +39,175 @@ public class DropTarget extends Activity {
         View view = getLayoutInflater().inflate(R.layout.main_activity, null);
         setContentView(view);
 
-        setUpDropTarget(R.id.dont_request, false);
-        setUpDropTarget(R.id.do_request, true);
+        setUpDropTarget(R.id.dont_request, new OnDragUriReadListener(false));
+        setUpDropTarget(R.id.request_read, new OnDragUriReadListener());
+        setUpDropTarget(R.id.request_write, new OnDragUriWriteListener());
+        setUpDropTarget(R.id.request_read_nested, new OnDragUriReadPrefixListener());
+        setUpDropTarget(R.id.request_take_persistable, new OnDragUriTakePersistableListener());
     }
 
-    private void setUpDropTarget(final int targetResourceId,
-                                 final boolean requestPermissions) {
-        findViewById(targetResourceId).setOnDragListener(new View.OnDragListener() {
-            @Override
-            public boolean onDrag(View v, DragEvent event) {
-                switch (event.getAction()) {
-                    case DragEvent.ACTION_DRAG_STARTED:
-                        return true;
+    private void setUpDropTarget(final int targetResourceId, OnDragUriListener listener) {
+        findViewById(targetResourceId).setOnDragListener(listener);
+    }
 
-                    case DragEvent.ACTION_DRAG_ENTERED:
-                        return true;
+    private abstract class OnDragUriListener implements View.OnDragListener {
+        private final boolean requestPermissions;
 
-                    case DragEvent.ACTION_DRAG_LOCATION:
-                        return true;
+        public OnDragUriListener(boolean requestPermissions) {
+            this.requestPermissions = requestPermissions;
+        }
 
-                    case DragEvent.ACTION_DRAG_EXITED:
-                        return true;
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    ((TextView) findViewById(R.id.drag_started)).setText("DRAG_STARTED");
+                    return true;
 
-                    case DragEvent.ACTION_DROP:
-                        String result;
-                        try {
-                            result = processDrop(event, requestPermissions);
-                        } catch (Exception e) {
-                            result = "Exception";
-                            ((TextView) findViewById(R.id.details)).setText(e.getMessage());
-                        }
-                        ((TextView) findViewById(R.id.result)).setText(result);
-                        return true;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    return true;
 
-                    case DragEvent.ACTION_DRAG_ENDED:
-                        return true;
+                case DragEvent.ACTION_DRAG_LOCATION:
+                    return true;
 
-                    default:
-                        return false;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    return true;
+
+                case DragEvent.ACTION_DROP:
+                    String result;
+                    try {
+                        result = processDrop(event, requestPermissions);
+                    } catch (Exception e) {
+                        result = "Exception";
+                        ((TextView) findViewById(R.id.details)).setText(e.getMessage());
+                    }
+                    ((TextView) findViewById(R.id.result)).setText(result);
+                    return true;
+
+                case DragEvent.ACTION_DRAG_ENDED:
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+
+        private String processDrop(DragEvent event, boolean requestPermissions) {
+            final ClipData clipData = event.getClipData();
+            if (clipData == null) {
+                return "Null ClipData";
+            }
+            if (clipData.getItemCount() == 0) {
+                return "Empty ClipData";
+            }
+            ClipData.Item item = clipData.getItemAt(0);
+            if (item == null) {
+                return "Null ClipData.Item";
+            }
+            Uri uri = item.getUri();
+            if (uri == null) {
+                return "Null Uri";
+            }
+
+            DropPermissions dp = null;
+            if (requestPermissions) {
+                dp = requestDropPermissions(event);
+                if (dp == null) {
+                    return "Null DropPermissions";
                 }
             }
-        });
+
+            try {
+                return processUri(uri);
+            } finally {
+                if (dp != null) {
+                    dp.release();
+                }
+            }
+        }
+
+        abstract protected String processUri(Uri uri);
     }
 
-    private String processDrop(DragEvent event, boolean requestPermissions) {
-        final ClipData clipData = event.getClipData();
-        if (clipData == null) {
-           return "Null ClipData";
-        }
-        if (clipData.getItemCount() == 0) {
-           return "Empty ClipData";
-        }
-        ClipData.Item item = clipData.getItemAt(0);
-        if (item == null) {
-            return "Null ClipData.Item";
-        }
-        Uri uri = item.getUri();
-        if (uri == null) {
-            return "Null Uri";
+    private class OnDragUriReadListener extends OnDragUriListener {
+        public OnDragUriReadListener(boolean requestPermissions) {
+            super(requestPermissions);
         }
 
-        DropPermissions dp = null;
-        if (requestPermissions) {
-            dp = requestDropPermissions(event);
-            if (dp == null) {
-                return "Null DropPermissions";
-            }
+        public OnDragUriReadListener() {
+            super(true);
         }
 
-        Cursor cursor = null;
-        try {
-            cursor = getContentResolver().query(uri, null, null, null, null);
-            if (cursor == null) {
-                return "Null Cursor";
+        protected String processUri(Uri uri) {
+            return checkQueryResult(uri, MAGIC_VALUE);
+        }
+
+        protected String checkQueryResult(Uri uri, String expectedValue) {
+            Cursor cursor = null;
+            try {
+                cursor = getContentResolver().query(uri, null, null, null, null);
+                if (cursor == null) {
+                    return "Null Cursor";
+                }
+                cursor.moveToPosition(0);
+                String value = cursor.getString(0);
+                if (!expectedValue.equals(value)) {
+                    return "Wrong value: " + value;
+                }
+                return RESULT_OK;
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
             }
-            cursor.moveToPosition(0);
-            String value = cursor.getString(0);
-            if (!MAGIC_VALUE.equals(value)) {
-                return "Wrong value: " + value;
+        }
+    }
+
+    private class OnDragUriWriteListener extends OnDragUriListener {
+        public OnDragUriWriteListener() {
+            super(true);
+        }
+
+        protected String processUri(Uri uri) {
+            ContentValues values = new ContentValues();
+            values.put("key", 100);
+            getContentResolver().update(uri, values, null, null);
+            return RESULT_OK;
+        }
+    }
+
+    private class OnDragUriReadPrefixListener extends OnDragUriReadListener {
+        @Override
+        protected String processUri(Uri uri) {
+            final String result1 = queryPrefixed(uri, "1");
+            if (!result1.equals(RESULT_OK)) {
+                return result1;
             }
-            return "OK";
-        } finally {
-            if (cursor != null) {
-                cursor.close();
+            final String result2 = queryPrefixed(uri, "2");
+            if (!result2.equals(RESULT_OK)) {
+                return result2;
             }
-            if (dp != null) {
-                dp.release();
-            }
+            return queryPrefixed(uri, "3");
+        }
+
+        private String queryPrefixed(Uri uri, String selector) {
+            final Uri prefixedUri = Uri.parse(uri.toString() + "/" + selector);
+            return checkQueryResult(prefixedUri, selector);
+        }
+    }
+
+    private class OnDragUriTakePersistableListener extends OnDragUriListener {
+        public OnDragUriTakePersistableListener() {
+            super(true);
+        }
+
+        @Override
+        protected String processUri(Uri uri) {
+            getContentResolver().takePersistableUriPermission(
+                    uri, View.DRAG_FLAG_GLOBAL_URI_READ);
+            getContentResolver().releasePersistableUriPermission(
+                    uri, View.DRAG_FLAG_GLOBAL_URI_READ);
+            return RESULT_OK;
         }
     }
 }
