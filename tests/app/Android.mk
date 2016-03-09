f4123ebeae35f72e006c8695ed891787a5bccf79
Kirill Grouchnikov <kirillg@google.com>
2016-03-09 17:58:13 -0500

    Tidying up AlertDialog CTS tests
    * Switch to Mockito for mocking listeners and querying calls.
    * Much more targeted checks on how many times a listener has been
    called in the specific scenario and with which parameter(s).
    * Small cleanups in the code.
    
    Change-Id: Ieb4e3bef2d49c19b2a029730d4e6a3aba405f04b

--- tests/app/Android.mk
+++ tests/app/Android.mk
@@ -23,7 +23,7 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
 
 LOCAL_JAVA_LIBRARIES := android.test.runner telephony-common voip-common org.apache.http.legacy
 
-LOCAL_STATIC_JAVA_LIBRARIES := ctsdeviceutil ctstestrunner ctstestserver
+LOCAL_STATIC_JAVA_LIBRARIES := ctsdeviceutil ctstestrunner ctstestserver mockito-target
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
