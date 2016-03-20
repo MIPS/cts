845ec7d6f874749454acfe6a10e2cbe6d5c91116
Guang Zhu <guangzhu@google.com>
2016-03-20 11:15:25 -0700

    check device connectivity between test packages
    Change-Id: Ic517ec10830815aa0a37559591421329a5dfcee3

---
+++ common/host-side/util/src/com/android/compatibility/common/util/MonitoringUtils.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.compatibility.common.util;
+
+import com.android.tradefed.device.DeviceNotAvailableException;
+import com.android.tradefed.device.ITestDevice;
+import com.android.tradefed.log.LogUtil.CLog;
+import com.android.tradefed.result.ITestInvocationListener;
+import com.android.tradefed.result.InputStreamSource;
+import com.android.tradefed.result.LogDataType;
+
+/**
+ * Utility functions related to device state monitoring during compatibility test.
+ */
+public class MonitoringUtils {
+
+    private static final long CONNECTIVITY_CHECK_TIME_MS = 60 * 1000;
+    private static final long CONNECTIVITY_CHECK_INTERVAL_MS = 5 * 1000;
+
+    public static void checkDeviceConnectivity(ITestDevice device, ITestInvocationListener listener,
+            String tag) throws DeviceNotAvailableException {
+            long start = System.currentTimeMillis();
+            while (System.currentTimeMillis() - start < CONNECTIVITY_CHECK_TIME_MS) {
+                if (device.checkConnectivity()) {
+                    CLog.i("Connectivity: passed check.");
+                    return;
+                } else {
+                    try {
+                        Thread.sleep(CONNECTIVITY_CHECK_INTERVAL_MS);
+                    } catch (InterruptedException ie) {
+                        return;
+                    }
+                }
+            }
+            CLog.w("Connectivity: check failed.");
+            InputStreamSource bugSource = device.getBugreport();
+            listener.testLog(String.format("bugreport-connectivity-%s", tag),
+                    LogDataType.TEXT, bugSource);
+            bugSource.cancel();
+        }
+
+}
