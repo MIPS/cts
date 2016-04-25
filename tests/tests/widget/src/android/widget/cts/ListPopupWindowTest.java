10ab35ce9b82672ddb27d3f290533e95947e0535
Alan Viverette <alanv@google.com>
2016-04-25 14:04:12 -0400

    Wait for layout pass before checking item selection listener
    Bug: 28352146
    Change-Id: I446ef8506bc0e28bcfdc37e001888ca9aeca12d0

--- tests/tests/widget/src/android/widget/cts/ListPopupWindowTest.java
+++ tests/tests/widget/src/android/widget/cts/ListPopupWindowTest.java
@@ -595,6 +595,8 @@ public class ListPopupWindowTest extends
                 .withDismissListener().withItemSelectedListener();
         mPopupWindowBuilder.show();
 
+        final View root = mPopupWindow.getListView().getRootView();
+
         // "Point" our custom extension of EditText to our ListPopupWindow
         final MockViewForListPopupWindow anchor =
                 (MockViewForListPopupWindow) mPopupWindow.getAnchorView();
@@ -617,6 +619,8 @@ public class ListPopupWindowTest extends
         mInstrumentation.sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_DOWN);
         mInstrumentation.waitForIdleSync();
 
+        ViewTestUtils.runOnMainAndDrawSync(mInstrumentation, root, null);
+
         // At this point we expect that item #2 was selected
         verify(mPopupWindowBuilder.mOnItemSelectedListener, times(1)).onItemSelected(
                 any(AdapterView.class), any(View.class), eq(2), eq(2L));
@@ -627,6 +631,8 @@ public class ListPopupWindowTest extends
         mInstrumentation.sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_UP);
         mInstrumentation.waitForIdleSync();
 
+        ViewTestUtils.runOnMainAndDrawSync(mInstrumentation, root, null);
+
         // At this point we expect that item #1 was selected
         verify(mPopupWindowBuilder.mOnItemSelectedListener, times(2)).onItemSelected(
                 any(AdapterView.class), any(View.class), eq(1), eq(1L));
@@ -636,6 +642,8 @@ public class ListPopupWindowTest extends
         // up one more row
         mInstrumentation.sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_UP);
         mInstrumentation.waitForIdleSync();
+
+        ViewTestUtils.runOnMainAndDrawSync(mInstrumentation, root, null);
 
         // At this point we expect that item #0 was selected
         verify(mPopupWindowBuilder.mOnItemSelectedListener, times(1)).onItemSelected(
