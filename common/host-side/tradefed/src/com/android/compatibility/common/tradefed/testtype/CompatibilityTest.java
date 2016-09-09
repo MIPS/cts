b249785a461190bacbf35929fe16a89047a75378
Julien Desprez <jdesprez@google.com>
2016-09-09 15:17:17 +0100

    Fix module filtering when include filters exists
    When module (-m) is provided and include filters exist
    make sure the list is properly repopulated.
    
    Bug: 31370651
    Change-Id: I5629d373e9d0a9dc5b11db3d2e6ae85093965ff6

--- common/host-side/tradefed/src/com/android/compatibility/common/tradefed/testtype/CompatibilityTest.java
+++ common/host-side/tradefed/src/com/android/compatibility/common/tradefed/testtype/CompatibilityTest.java
@@ -650,7 +650,8 @@ public class CompatibilityTest implements IDeviceTest, IShardableTest, IBuildRec
                 cleanedFilters.add(filter); // Module name matches, filter passes
             }
         }
-        filters = cleanedFilters;
+        filters.clear();
+        filters.addAll(cleanedFilters);
     }
 
     /**
