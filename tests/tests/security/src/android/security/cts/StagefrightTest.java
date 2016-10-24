63558c8877e58846caebba2dd5b340ab0d5129ce
Marco Nelissen <marcone@google.com>
2016-10-24 13:21:05 -0700

    DO NOT MERGE: Test for vorbis decoder bug
    Bug: 31647370
    Test: ran test with and without vorbis fix
    Change-Id: Iefd4edd6d2f63517751ef6976bce1c42dbdf7886

--- tests/tests/security/src/android/security/cts/StagefrightTest.java
+++ tests/tests/security/src/android/security/cts/StagefrightTest.java
@@ -62,6 +62,10 @@ public class StagefrightTest extends InstrumentationTestCase {
     public StagefrightTest() {
     }
 
+    public void testStagefright_bug_31647370() throws Exception {
+        doStagefrightTest(R.raw.bug_31647370);
+    }
+
     public void testStagefright_bug_32577290() throws Exception {
         doStagefrightTest(R.raw.bug_32577290);
     }
