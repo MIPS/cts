ecfa904f0a59fbda78929a6cd8ca734ba6fcd59a
Wei Jia <wjia@google.com>
2017-04-26 16:09:05 -0700

    DO NOT MERGE: StagefrightTest: add test for bug 22771132
    Test: cts test passes
    Bug: 37225642
    Change-Id: I5b04c48f1ee14f3cffd46072d380f1ca2173a2e1
    (cherry picked from commit b5dec675186ded267a113bd8a9fedd5a5f5c2d07)

--- tests/tests/security/src/android/security/cts/StagefrightTest.java
+++ tests/tests/security/src/android/security/cts/StagefrightTest.java
@@ -67,6 +67,10 @@ public class StagefrightTest extends InstrumentationTestCase {
      before any existing test methods
      ***********************************************************/
 
+    public void testStagefright_bug_22771132() throws Exception {
+        doStagefrightTest(R.raw.bug_22771132);
+    }
+
     public void testStagefright_bug_21443020() throws Exception {
         doStagefrightTest(R.raw.bug_21443020_webm);
     }
